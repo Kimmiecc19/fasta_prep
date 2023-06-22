4ac8238b02cf398dacf063ae221ab43571e95fd9
Kimberly Coetzer <78590097+Kimmiecc19@users.noreply.github.com>
2023-06-22 15:23:10 +0200

    Update add_contams_to_hrp.py
    Completely changed script.

--- add_contams_to_hrp.py
+++ add_contams_to_hrp.py
@@ -1,72 +1,78 @@
-#Import the necessary modules
+#load modules that will be used
 import os
-import sys
-import logging
-import subprocess
+import requests
 
-# Run get_fasta.py script
-subprocess.run(['python', 'get_fasta.py'])
 
-# Run get_cRAP.py script
-subprocess.run(['python', 'get_cRAP.py'])
+#Defines the function
+def download_file(url, output_path):
+	#Sends an HTTP GET request to the provided url using the requests library. The response from the server is stored in the response variable
+    response = requests.get(url)
+    #Checks if the HTTP response status code is 200, which indicates a successful request. If the condition is true, it means the file was successfully retrieved from the server.
+    if response.status_code == 200:
+    	#Opens the output_path file in write mode ('w') using a context manager
+        with open(output_path, 'w') as file:
+        	#Writes the content of the response (the downloaded file) to the opened file
+            file.write(response.text)
+            #Print message if successfully saved to output_path
+        print(f"File saved as '{output_path}'")
+    else:
+    	#Prints an error message with status code
+        print("Error occurred while retrieving the file:", response.status_code)
 
-# Set up logging
-#Send output to a log file (log_fasta_contam.txt)
-loglevel = 'DEBUG'  # Replace with the desired log level
-numeric_level = getattr(logging, loglevel.upper(), None)
-if not isinstance(numeric_level, int):
-    raise ValueError('Invalid log level: %s' % loglevel)
 
-logging.basicConfig(format='%(asctime)s %(message)s', level=numeric_level, filename='log_fasta_contam.txt')
-log = logging.getLogger("fasta_contam")
-log.info("This is when this event was logged.")
-log.warning("Something is not quite right.")
-logging.debug('Try again.')
-logging.error('Something went wrong.')
+#Define the function
+def merge_fasta_files(fasta_file_path, crap_file_path, output_file_path):
+	#Opens the fasta_file_path file in read mode ('r') using a context manager
+    with open(fasta_file_path, 'r') as file:
+    	#Reads the contents of the opened fasta_file_path file and assigns it to the variable fasta_content
+        fasta_content = file.read()
 
-# Open the input FASTA file
+    #Opens the crap_file_path file in read mode ('r') using a context manager
+    with open(crap_file_path, 'r') as file:
+    	#Reads the contents of the opened crap_file_path file and assigns it to the variable crap_content
+        crap_content = file.read()
+
+    #splits the crap_content string into a list of entries based on the ">" delimiter. 
+    #Each entry represents a sequence in the FASTA format
+    crap_entries = crap_content.split(">")
+
+    #Starts a loop that iterates over each entry in the crap_entries list
+    for entry in crap_entries:
+    	#Checks if the current entry is empty or consists only of whitespace characters. 
+    	#If it is empty, the loop proceeds to the next iteration using the continue statement.
+        if not entry.strip():
+            continue
+        
+        #Splits the current entry into a list of lines based on the newline character ("\n")
+        lines = entry.split("\n")
+        #Assigns the first line of the entry to the variable header. 
+        #This line is assumed to contain the header information of the sequence
+        header = lines[0]
+        #Joins the remaining lines (starting from the second line) of the entry into a single string, representing the sequence. 
+        #The empty string ('') is used as the separator between lines.
+        sequence = ''.join(lines[1:])
+
+        #Appends the merged content of each entry to the fasta_content string. 
+        #The ">" symbol, header, and sequence are formatted as a single string.
+        fasta_content += f">{header}\n{sequence}\n"
+    
+    #Opens the output_file_path file in write mode ('w') using a context manager
+    with open(output_file_path, 'w') as file:
+    	#Writes the contents of the fasta_content string to the opened output_file_path file
+        file.write(fasta_content)
+
+#Specify URLS and file names to be used
+fasta_url = 'https://rest.uniprot.org/uniprotkb/stream?format=fasta&query=%28%28proteome%3AUP000005640%29%29'
+crap_url = 'http://ftp.thegpm.org/fasta/cRAP/crap.fasta'
 fasta_file_path = "HRP.fasta"
-with open(fasta_file_path, 'r') as file:
-# Read the existing content of the FASTA file
-    fasta_content = file.read()
-print("Opening input FASTA file")
-# Read the cRAP contaminants
 crap_file_path = "crap.fasta"
-with open(crap_file_path, 'r') as file:
-    # Read the cRAP FASTA content
-    crap_content = file.read()
-print("Reading cRAP FASTA file")
-# Split the cRAP content into individual entries
-#This line splits the crap_content string into individual entries based on the ">" delimiter and stores the result in the crap_entries list.
-crap_entries = crap_content.split(">")
+output_file_path = "output.fasta"
 
-#This block iterates through each entry in the crap_entries list. 
-#It first checks if the entry is empty or contains only whitespace. 
-#If yes, it skips that entry and moves to the next one.
-# Process and append cRAP contaminants
-for entry in crap_entries:
-    # Skip empty entries
-    if not entry.strip():
-        continue
-#For non-empty entries, it splits the entry into a header and sequence using the first occurrence of the newline character (\n). 
-#The sequence is then modified to remove any newline characters. 
-#The header and modified sequence are concatenated with the existing fasta_content variable.
-    # Split the entry into header and sequence
-    header, sequence = entry.split("\n", 1)
-    sequence = sequence.replace("\n", "")
+# Download FASTA file
+download_file(fasta_url, fasta_file_path)
 
-    # Append the cRAP contaminant to the FASTA content
-    fasta_content += f">{header}\n{sequence}\n"
+# Download contaminant file
+download_file(crap_url, crap_file_path)
 
-# Write the updated content to a new FASTA file
-output_file_path = "hrp_and_contams.fasta"
-with open(output_file_path, 'w') as file:
-    file.write(fasta_content)
-print("New FASTA file with contaminants made")
-
-#This block of code checks if the script is being executed as the main module.
-if __name__ == '__main__':
-    contam_file = ''  # Assign the path to the contaminants file
-    fasta_file = ''  # Assign the path to the input FASTA file
-    output_file = ''  # Assign the path to the output FASTA file
-
+# Merge FASTA files into one file
+merge_fasta_files(fasta_file_path, crap_file_path, output_file_path)
